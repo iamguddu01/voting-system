c57a287deeec50774c4573d88aa24e745f28dfb5
Govind Kumar <govind.byte@gmail.com>
2026-06-08 19:22:19 +0530

    auth middleware

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,4 +1,12 @@
-export const checkUserAuth = async(req, res) => {
-    const token = req.header.authorization;
-    
+import jwt from "jsonwebtoken"
+export const checkUserAuth = async(req, res, next) => {
+    const token = req.headers.authorization;
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY)
+    if(!decoded?._doc){
+        res.status(403).json({
+            message: "User is not authenticated"
+        })
+    }
+    req.user = decoded?._doc
+    next()
 }
--- src/routes/team.route.js
+++ src/routes/team.route.js
@@ -1,11 +1,12 @@
 import express from "express"
 import { createTeam, deleteTeam, getAllTeam, getTeamDetailsByCode, updateTeam } from "../controllers/team.controller.js";
+import { checkUserAuth } from "../middlewares/index.js";
 
 const router = express.Router();
 // create
-router.post("/create-team", createTeam)
-router.get("/get-all-teams", getAllTeam)
-router.post("/get-team-details/:code", getTeamDetailsByCode)
-router.delete("/delete-team/:code", deleteTeam)
-router.put("/update-team/:teamId", updateTeam)
+router.post("/create-team", checkUserAuth, createTeam)
+router.get("/get-all-teams", checkUserAuth, getAllTeam)
+router.post("/get-team-details/:code", checkUserAuth, getTeamDetailsByCode)
+router.delete("/delete-team/:code", checkUserAuth, deleteTeam)
+router.put("/update-team/:teamId", checkUserAuth, updateTeam)
 export default router;
